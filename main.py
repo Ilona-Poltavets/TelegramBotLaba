0eb8a5d9c4cae1182fedf7cf9999fa92c71a6ec6
Ilona Poltavets <20ilona.poltavets02@gmail.com>
2024-04-08 01:37:28 +0300

    bot init

---
+++ main.py
@@ -0,0 +1,11 @@
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(message.chat.id, "Hello, World")
+
+bot.polling(none_stop=True)
